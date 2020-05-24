37ea11acd0576362fad93d86d02a12013a46c354
Danikho88 <danikho@gmail.com>
2020-05-24 00:24:15 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,94 @@
+from flask import Flask, request
+from flask import jsonify
+from config import config
+from models import db, User
+from decouple import config as config_decouple
+
+def create_app(enviroment):
+    app = Flask(__name__)
+
+    app.config.from_object(enviroment)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+    return app
+
+enviroment = config['development']
+if config_decouple('PRODUCTION', default=False):
+    enviroment = config['production']
+
+app = create_app(enviroment)
+
+
+
+
+@app.route('/')
+def index():
+    response = {'message': 'coso'}
+    return jsonify(response)
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    json = request.get_json(force=True)
+
+    if json.get('username') is None:
+        return jsonify({'message': 'Bad request'}), 400
+
+    user = User.create(json['username'])
+
+    return jsonify({'user': user.json() })
+    
+
+
+
+@app.route('/api/v1/users/<id>', methods=['GET'])
+def get_user(id):
+    user = User.query.filter_by(id=id).first()
+    if user is None:
+        return jsonify({'message': 'User does not exists'}), 404
+
+    return jsonify({'user': user.json() })
+
+
+
+@app.route('/api/v1/users/', methods=['POST'])
+def create_user():
+    json = request.get_json(force=True)
+
+    if json.get('username') is None:
+        return jsonify({'message': 'Bad request'}), 400
+
+    user = User.create(json['username'])
+
+    return jsonify({'user': user.json() })
+
+@app.route('/api/v1/users/<id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.filter_by(id=id).first()
+    if user is None:
+            return jsonify({'message': 'User does not exists'}), 404
+
+    json = request.get_json(force=True)
+    if json.get('username') is None:
+            return jsonify({'message': 'Bad request'}), 400
+
+    user.username = json['username']
+
+    user.update()
+
+    return jsonify({'user': user.json() })
+
+@app.route('/api/v1/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.filter_by(id=id).first()
+    if user is None:
+        return jsonify({'message': 'User does not exists'}), 404
+
+    user.delete()
+
+    return jsonify({'user': user.json() })
+
+if __name__ == '__main__':
+    app.run(debug=True)
